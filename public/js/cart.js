ddf389e7ea08c424bd22cf7f6035e0f1ef4069f9
josevitor555 <koservitor123@gmail.com>
2024-05-03 09:09:23 -0300

    Cannot GET /[object%20Object]

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,8 +1,8 @@
 const payBtn = document.querySelector(".btn-buy");
 
 payBtn.addEventListener("click", () => {
-  fetch("http://localhost:3000/stripe-checkout", {
-    method: "post",
+  fetch('/stripe-checkout', {
+    method: "POST",
     headers: new Headers({
       "Content-Type": "application/json"
     }),
@@ -17,19 +17,16 @@ payBtn.addEventListener("click", () => {
     .catch((error) => console.error(error));
 });
 
-
-// Cookies blocked:
-
-// import { Stripe } from 'stripe';
+// import { Stripe } from "stripe";
 
 // var stripe = Stripe(
-//   "keypk_live_51P9d4GJTiGK3Mb2UwyL8RGw6jnWIM77UN2yNujCL14Hda1vsmi6j3itSGpj7oSrUI44CMGkjqfSjwwaJGu9TPJml00LRaUSErj"
+//   "pk_test_51P9d4GJTiGK3Mb2UmpPGmNUzlzaSD2bwUpUMmCFxFU9ZwwQucXzCy8NdiQ6DganlT32P22hnmf1bFdqhKhTOscKI00Gq1jRbss"
 // );
 // var checkoutButton = document.querySelector(".btn-buy");
 
 // checkoutButton.addEventListener("click", function () {
 //   fetch("/stripe-checkout", {
-//     headers: { "Content-Type": "application/json" },
+//     headers: new Headers({ "Content-Type": "application/json" }),
 //     method: "POST",
 //     body: JSON.stringify({
 //       items: JSON.parse(localStorage.getItem("cartItems")) || [],
--- server.js
+++ server.js
@@ -32,54 +32,55 @@ const StripeGateway = stripe(process.env.STRIPE_SECRET_KEY);
 // console.log(`Stripe API: ${process.env.STRIPE_SECRET_KEY}`);
 
 const DOMAIN = process.env.DOMAIN;
+console.log(typeof DOMAIN, DOMAIN);
+
 // console.log(`Running on ${process.env.DOMAIN}`);
 
 app.post('/stripe-checkout', async (request, response) => {
-    // try {
-    //     // const { items } = request.body;
+    try {
+        // const { items } = request.body;
 
-    //     // if (!items || !Array.isArray(items) || items.length === 0) {
-    //     //     return response.status(400).json({ error: 'No items in the cart.' });
-    //     // }
+        // if (!items || !Array.isArray(items) || items.length === 0) {
+        //     return response.status(400).json({ error: 'No items in the cart.' });
+        // }
 
-        
-        
-    // } catch (error) {
-    //     console.error('Error creating checkout session:', error);
-    //     response.status(500).json({ error: 'Failed to create checkout session' });
-    // }
-
-    const lineItems = request.body.items.map((item) => {
-        const unitAmount = Math.round(parseFloat(item.price.replace(/[^\d.]/g, '')) * 100);
-        console.log(`item-price: ${item.price}`);
-        console.log(`unitAmount: ${unitAmount}`);
-
-        return {
-            price_data: {
-                currency: 'brl',
-                product_data: {
-                    name: item.title,
-                    images: [item.productImg]
+        const lineItems = request.body.items.map((item) => {
+            const unitAmount = Math.round(parseFloat(item.price.replace(/[^\d.]/g, '')) * 100);
+            console.log(`item-price: ${item.price}`);
+            console.log(`unitAmount: ${unitAmount}`);
+    
+            return {
+                price_data: {
+                    currency: 'brl',
+                    product_data: {
+                        name: item.title,
+                        images: [item.productImg]
+                    },
+                    unit_amount: unitAmount
                 },
-                unit_amount: unitAmount
-            },
-            quantity: item.quantity
-        };
-    });
-    console.log(`lineItems: ${JSON.stringify(lineItems)}`);
-
-    // Create Checkout Session
-    const session = await StripeGateway.checkout.sessions.create({
-        payment_method_types: ['card'],
-        success_url: `${DOMAIN}/successPage`,
-        cancel_url: `${DOMAIN}/failedPage`,
-        line_items: lineItems,
-        billing_address_collection: 'required'
-    });
-    response.json({ url: session.url });
+                quantity: item.quantity
+            };
+        });
+        console.log(`lineItems: ${JSON.stringify(lineItems)}`);
+    
+        // Create Checkout Session
+        const session = await StripeGateway.checkout.sessions.create({
+            payment_method_types: ['card'],
+            success_url: `${DOMAIN}/successPage.html`,
+            cancel_url: `${DOMAIN}/failedPage.html`,
+            line_items: lineItems,
+            billing_address_collection: 'required'
+        });
+        response.json({ url: session.url });
+        
+    } catch (error) {
+        console.error('Error creating checkout session:', error);
+        response.status(500).json({ error: 'Failed to create checkout session' });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT} PORT.`);
